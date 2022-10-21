e1d19e9ff708d4b2a461540862f224bb8421f877
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-10-21 09:28:10 +0100

    add navbar to home page done

--- code/client/src/components/navBar/NavBar.jsx
+++ code/client/src/components/navBar/NavBar.jsx
@@ -3,31 +3,31 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import "./styles.css";
 
 function Navbar() {
-	const navRef = useRef();
+  const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
-	};
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-	return (
-		<header>
-			<h3>LOGO</h3>
-			<nav ref={navRef}>
-				<a href="/#">Home</a>
-				<a href="/#">My work</a>
-				<a href="/#">Blog</a>
-				<a href="/#">About me</a>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button className="nav-btn" onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+  return (
+    <header>
+      <h3>LOGO</h3>
+      <nav ref={navRef}>
+        <a href="/#">Home</a>
+        <a href="/#">My work</a>
+        <a href="/#">Blog</a>
+        <a href="/#">About me</a>
+		<button className="res-sign bg-red-500">sign in</button>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="sign bg-red-500">sign in</button>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
 
 export default Navbar;
