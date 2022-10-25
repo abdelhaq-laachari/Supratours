98a1c5e8de75d0a717291734a5f15c4dc3843bbe
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-10-25 17:08:45 +0100

    finlly i finish just the search input for trips

--- code/client/src/components/datePicker/datePicker.jsx
+++ code/client/src/components/datePicker/datePicker.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import {LocalizationProvider} from '@mui/lab'
+import * as React from 'react';
+import dayjs from 'dayjs';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+// import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
-const datePicker = () => {
+export default function ResponsiveDatePickers() {
+  const [value, setValue] = React.useState(dayjs(new Date()));
+  console.log(value);
+
   return (
-    <div>datePicker</div>
-  )
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Stack spacing={3}>
+        <MobileDatePicker
+          label="Select date start"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+          minDate={new Date()}
+        />
+      </Stack>
+    </LocalizationProvider>
+  );
 }
-
-export default datePicker
--- code/client/src/pages/Home.jsx
+++ code/client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "../components/navBar/NavBar";
 import SearchBox from "../components/Search/SearchBox";
 import Background from "../components/video/Background";
-import DatePicker from "../components/datePicker/datePicker";
+import DatePicker from "../components/datePicker/DatePicker";
 import "../styles/Home.css";
 const Home = () => {
   return (
@@ -10,28 +10,32 @@ const Home = () => {
       <Background />
       <div className="main">
         <NavBar />
-        <form className="w-full max-w-lg">
-          <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        <h1 className="absolute lg:top-60 top-52 text-center  lg:text-center text-white lg:text-3xl">
+          Join <span className="lg:text-3xl font-serif bold text-red-300 ">Supr@tours</span> to explore the best locations in <span className="lg:text-3xl font-serif bold text-red-300 ">Morocco</span>.
+        </h1>
+        <form className="absolute lg:top-2/4 top-1/3 flex justify-center bg-white p-3 items-center w-11/12 rounded-md shadow-lg ">
+          <div className="flex items-center flex-wrap -mx-3 mb-2">
+            <div className="w-full z-20 md:w-1/3 px-3 mb-6 md:mb-0">
               <label
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 htmlFor="grid-city"
               >
                 From
               </label>
-              <SearchBox/>
+              <SearchBox />
             </div>
-            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <div className="w-full z-10 md:w-1/3 px-3 mb-6 md:mb-0">
               <label
                 className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                 htmlFor="grid-city"
               >
                 From
               </label>
-              <SearchBox/>
+              <SearchBox />
             </div>
-            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <DatePicker/>
+            <div className="flex space-x-5 w-full md:w-1/3 px-3 mb-6 md:mb-0">
+              <DatePicker />
+              <DatePicker />
             </div>
           </div>
         </form>
