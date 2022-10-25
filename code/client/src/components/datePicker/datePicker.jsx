0a07834af9760f7a2637d97d39c7e142e93627bd
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-10-25 14:18:56 +0100

    install mui x components

---
+++ code/client/src/components/datePicker/datePicker.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {LocalizationProvider} from '@mui/lab'
+
+const datePicker = () => {
+  return (
+    <div>datePicker</div>
+  )
+}
+
+export default datePicker
--- code/client/src/pages/Home.jsx
+++ code/client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../components/navBar/NavBar";
 import SearchBox from "../components/Search/SearchBox";
 import Background from "../components/video/Background";
+import DatePicker from "../components/datePicker/datePicker";
 import "../styles/Home.css";
 const Home = () => {
   return (
@@ -30,18 +31,7 @@ const Home = () => {
               <SearchBox/>
             </div>
             <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                htmlFor="grid-zip"
-              >
-                Zip
-              </label>
-              <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="grid-zip"
-                type="text"
-                placeholder="90210"
-              />
+              <DatePicker/>
             </div>
           </div>
         </form>
