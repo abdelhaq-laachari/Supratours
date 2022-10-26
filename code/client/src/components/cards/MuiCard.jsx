6299762c4bbe2c750d6f0d743bb709cf49675747
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-10-26 21:29:06 +0100

    i just add a card for most popular routes

--- code/client/src/components/cards/MuiCard.jsx
+++ code/client/src/components/cards/MuiCard.jsx
@@ -17,15 +17,16 @@ export default function ImgMediaCard(props) {
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {props.city}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {props.about}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          From {props.price}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Share</Button>
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
--- code/client/src/pages/Home.jsx
+++ code/client/src/pages/Home.jsx
@@ -9,9 +9,9 @@ import Image1 from "../assets/svg/image1.svg";
 import Image2 from "../assets/svg/image2.svg";
 import Image3 from "../assets/svg/image3.svg";
 import MuiCard from "../components/cards/MuiCard";
-import Mar from "../assets/img/mar.jpg"
-import Casa from "../assets/img/casa.jpg"
-import Rabat from "../assets/img/rab2.jpg"
+import Mar from "../assets/img/mar.jpg";
+import Casa from "../assets/img/casa.jpg";
+import Rabat from "../assets/img/rab2.jpg";
 
 const Home = () => {
   return (
@@ -20,20 +20,20 @@ const Home = () => {
       <section className="section1">
         <div className="main">
           <NavBar />
-          <h1 className="absolute lg:top-60 top-52 text-center  lg:text-center text-white lg:text-3xl">
+          <h1 className="absolute md:top-60 top-52 text-center text-2xl md:text-center text-white md:text-3xl">
             Join{" "}
-            <span className="lg:text-3xl font-serif bold text-red-300 ">
+            <span className="md:text-3xl font-serif bold text-red-300 ">
               Supr@tours
             </span>{" "}
             to explore the best locations in{" "}
-            <span className="lg:text-3xl font-serif bold text-red-300 ">
+            <span className="md:text-3xl font-serif bold text-red-300 ">
               Morocco
             </span>
             .
           </h1>
-          <form className="absolute lg:top-2/4 top-1/3 flex justify-center bg-white p-3 items-center w-10/12 rounded-md shadow-lg ">
-            <div className="flex lg:w-full items-center flex-wrap mx-3 p-2 justify-center">
-              <div className="w-full z-20 lg:w-1/4 px-3 mb-6 md:mb-0">
+          <form className="absolute md:top-2/4 top-1/3 flex justify-center bg-white p-3 items-center w-10/12 rounded-md shadow-lg ">
+            <div className="flex md:w-full items-center flex-wrap mx-3 p-2 justify-center">
+              <div className="w-full z-20 md:w-1/4 px-3 mb-6 md:mb-0">
                 <label
                   className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                   htmlFor="grid-city"
@@ -42,7 +42,7 @@ const Home = () => {
                 </label>
                 <SearchBox />
               </div>
-              <div className="w-full z-10 lg:w-1/4 px-3 mb-6 md:mb-0">
+              <div className="w-full z-10 md:w-1/4 px-3 mb-6 md:mb-0">
                 <label
                   className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                   htmlFor="grid-city"
@@ -51,17 +51,17 @@ const Home = () => {
                 </label>
                 <SearchBox />
               </div>
-              <div className="w-full lg:w-1/4 px-2 mb-6 md:mb-0">
+              <div className="w-full md:w-1/4 px-2 mb-6 md:mb-0">
                 <DatePicker />
               </div>
-              <button className="bg-red-400 hover:bg-red-500 text-white w-full lg:max-w-max font-bold py-2 px-4 rounded-full">
+              <button className="bg-red-400 hover:bg-red-500 text-white w-full md:max-w-max font-bold py-2 px-4 rounded-full">
                 Find Ticket
               </button>
             </div>
           </form>
         </div>
       </section>
-      <section className="flex lg:space-y-8 lg:flex-col lg:pt-7 lg:items-center h-screen bg-gray-50">
+      <section className="flex md:space-y-8 md:flex-col md:pt-7 md:items-center h-screen bg-gray-50">
         <div className="divide-y-4 text-3xl text-black font-bold text-center pt-5">
           <h1 className="">
             Get your ticket <span className="text-red-400">fast</span> and{" "}
@@ -73,21 +73,38 @@ const Home = () => {
           You can search all of Morocco's destinations with Supr@Tours <br />{" "}
           thanks to its more than 100 trips, <br /> so you'll never get lost.
         </p>
-        <div className="flex w-11/12 lg:flex-row lg:justify-center lg:space-x-6">
+        <div className="flex w-11/12 md:flex-row md:justify-center md:space-x-6">
           <Card2 src={Image1} msg="Book your bus ticket easily" />
           <Card2 src={Image2} msg="Find your next destination" />
           <Card2 src={Image3} msg="We're here for you 24/7" />
         </div>
-        <div>
-          <h1>Our most popular routes</h1>
-          <p>Explore </p>
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl font-semibold md:mt-8 md:text-3xl ">
+            Our most popular routes
+          </h1>
+          <p className="font-thin md:text-xl md:mt-5">
+            Visit our most popular route by booking your ticket online with{" "}
+            <span>supratours</span>.
+          </p>
         </div>
       </section>
       <section className="h-screen bg-slate-100">
-        <div className="flex lg:flex-row lg:w-full lg:justify-around lg:items-center ">
-          <MuiCard src={Mar} />
-          <MuiCard src={Casa} />
-          <MuiCard src={Rabat} />
+        <div className="flex md:flex-row md:w-full md:justify-around md:items-center ">
+          <MuiCard
+            src={Mar}
+            city="Marrakech"
+            about="Marrakesh, a former imperial city in western Morocco, is a major economic center and home to mosques, palaces and gardens. The medina is a densely packed, walled medieval city dating to the Berber Empire."
+          />
+          <MuiCard
+            src={Rabat}
+            city="Casablanca"
+            about="Rabat, Morocco's capital, rests along the shores of the Bouregreg River and the Atlantic Ocean. It's known for landmarks that speak to its Islamic and French-colonial heritage, including the Kasbah of the Udayas"
+          />
+          <MuiCard
+            src={Casa}
+            city="Rabat"
+            about="Casablanca is a port city and commercial hub in western Morocco, fronting the Atlantic Ocean. The city's French colonial legacy is seen in its downtown Mauresque architecture, a blend of Moorish style and European art deco"
+          />
         </div>
       </section>
     </>
