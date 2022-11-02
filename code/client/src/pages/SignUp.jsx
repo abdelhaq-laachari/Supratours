85fbb4fe78d5ad2e6d4e75c15cc811ccb9096c88
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-02 20:26:23 +0100

    working on register form

--- code/client/src/pages/SignUp.jsx
+++ code/client/src/pages/SignUp.jsx
@@ -2,21 +2,22 @@ import React from "react";
 import Home from "./Home";
 import { Link } from "react-router-dom";
 import SignIn from "./SignIn";
+import '../styles/SignUp.css'
 
 const SignUp = () => {
   return (
     <>
-      <div className="flex justify-center h-screen w-full bg-slate-800">
-        <section className=" dark:bg-gray-900 md:w-full ">
-          <div className="flex flex-col justify-center items-center px-6 py-8 mx-auto h-screen lg:py-0">
-            <div className=" bg-white rounded-lg shadow dark:border md:mt-0 sm:w-1/2 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="section1 flex justify-center md:h-screen w-full bg-slate-800">
+        <section className="w-full dark:bg-gray-900 ">
+          <div className="w-full flex flex-col justify-center items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:w-1/2 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
               <div className="w-full p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Create your account
                 </h1>
-                <form className="space-y-4 md:space-y-2" action="#">
-                  <div className="flex flex-col md:space-x-4 md:flex-row">
-                    <div>
+                <form className="space-y-4 md:space-y-2" >
+                  <div className="flex flex-col w-full  md:space-x-4 md:flex-row">
+                    <div className="w-full md:w-1/2">
                       <label
                         htmlFor="email"
                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -32,7 +33,7 @@ const SignUp = () => {
                         required=""
                       />
                     </div>
-                    <div>
+                    <div className="w-full md:w-1/2">
                       <label
                         htmlFor="email"
                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -49,8 +50,8 @@ const SignUp = () => {
                       />
                     </div>
                   </div>
-                  <div className="flex flex-col md:space-x-4 md:flex-row">
-                    <div>
+                  <div className="flex flex-col w-full md:space-x-4 md:flex-row">
+                    <div className="w-full md:w-1/2">
                       <label
                         htmlFor="email"
                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -66,7 +67,7 @@ const SignUp = () => {
                         required=""
                       />
                     </div>
-                    <div>
+                    <div className="w-full md:w-1/2">
                       <label
                         htmlFor="password"
                         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -78,6 +79,74 @@ const SignUp = () => {
                         name="password"
                         id="password"
                         placeholder="••••••••"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required=""
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col w-full md:space-x-4 md:flex-row">
+                    <div className="w-full md:w-1/2">
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Address line
+                      </label>
+                      <input
+                        type="address"
+                        name="address"
+                        id="address"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="5357 Layla Passage"
+                        required=""
+                      />
+                    </div>
+                    <div className="w-full md:w-1/2">
+                      <label
+                        htmlFor="city"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        City
+                      </label>
+                      <input
+                        type="city"
+                        name="city"
+                        id="city"
+                        placeholder="London"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required=""
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col w-full md:space-x-4 md:flex-row">
+                    <div className="w-full md:w-1/2">
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Address line
+                      </label>
+                      <input
+                        type="address"
+                        name="address"
+                        id="address"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="5357 Layla Passage"
+                        required=""
+                      />
+                    </div>
+                    <div className="w-full md:w-1/2">
+                      <label
+                        htmlFor="city"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        City
+                      </label>
+                      <input
+                        type="city"
+                        name="city"
+                        id="city"
+                        placeholder="London"
                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         required=""
                       />
