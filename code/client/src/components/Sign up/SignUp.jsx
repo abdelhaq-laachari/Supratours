69d10ad0345d4b33d55e847b68a57cafb394458f
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-02 15:56:48 +0100

    working on register form

---
+++ code/client/src/components/Sign up/SignUp.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import SignIn from "../../pages/SignIn";
+// import SignIn from "./SignIn";
+import { Link } from "react-router-dom";
+import Home from "../../pages/Home";
+// import Home from "./Home";
+import User from "../../assets/animated/user.json";
+// import User from "../assets/animated/user.json";
+import Lottie from "lottie-react";
+
+const SignUp = () => {
+  return (
+    <div className="flex justify-center h-screen w-full bg-slate-800">
+      <section className=" dark:bg-gray-900 md:w-full ">
+        <div className="flex flex-col justify-center items-center px-6 py-8 mx-auto h-screen lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Create your account
+              </h1>
+              <form className="space-y-4 md:space-y-2" action="#">
+                <div className="flex space-x-4">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      First name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Alex"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Last name
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Parker"
+                      required=""
+                    />
+                  </div>
+                </div>
+                <div className="flex space-x-6">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center justify-between"></div>
+                <button
+                  type="submit"
+                  className="w-full text-white border bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Sign up
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  You already have an account?{" "}
+                  <Link to="/signIn" element={<SignIn />}>
+                    <span className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                      Sign in
+                    </span>
+                  </Link>
+                </p>
+                <Link to="/" element={<Home />}>
+                  <span className="font-medium text-center text-primary-600  hover:underline dark:text-primary-500">
+                    Go Home
+                  </span>
+                </Link>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SignUp;
