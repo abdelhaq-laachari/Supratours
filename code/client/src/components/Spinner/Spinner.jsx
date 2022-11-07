05bfe024f21feca450986e5a0ab038cb4eaac331
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-07 22:50:59 +0100

    i just add a spinner

--- code/client/src/components/Spinner/Spinner.jsx
+++ code/client/src/components/Spinner/Spinner.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import "./Spinner.css";
+// import ClipLoader from "react-spinners/ClipLoader";
+import ClockLoader from "react-spinners/ClockLoader";
+import RingLoader from "react-spinners/RingLoader";
+import FadeLoader from "react-spinners/FadeLoader";
+
 
 export default function LoadingSpinner() {
   return (
     <div className="spinner-container">
-      <div className="loading-spinner">
-      </div>
+      <FadeLoader color="#36d7b7" />
     </div>
   );
 }
