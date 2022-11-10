f83d24ba9d0264a64b88daf74611f7bb97ee9898
Abdelhaq laachari <93976987+abdelhaq-laachari@users.noreply.github.com>
2022-11-10 14:56:24 +0100

    booking card look fine but i still need to work on it

--- code/client/src/components/Bus Card/BusCard.jsx
+++ code/client/src/components/Bus Card/BusCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Ctm from "../../assets/bus/ctm1.jpg";
 import { BiBus } from "react-icons/bi";
-import {AiOutlineClockCircle} from "react-icons/ai"
+import { AiOutlineClockCircle } from "react-icons/ai";
 import NewCard from "./new";
 
 const BusCard = () => {
   return (
     <>
       <div className="flex justify-center">
-        <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+        <div className="flex flex-col md:flex-row md:w-max rounded-lg bg-white shadow-lg">
           <img
             className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
             src={Ctm}
@@ -19,130 +19,39 @@ const BusCard = () => {
               <BiBus size={30} />
               <h5 className="text-black text-xl font-semibold">CTM Company</h5>
             </div>
-            {/* <div>
-              <ol className="relative border-l border-gray-200 dark:border-gray-700">
-                <li className="mb-10 ml-6">
-                  <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <BiBus
-                      className="text-blue-600 dark:text-blue-400"
-                      size={15}
-                    />
-                  </span>
-                  <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                    From Safi
-                    <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                      Latest
-                    </span>
-                  </h3>
-                  <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Departure time, 08:15
-                  </time>
-                </li>
-                <li className="mb-10 ml-6">
-                  <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <BiBus
-                      className="text-blue-600 dark:text-blue-400"
-                      size={15}
-                    />
-                  </span>
-                  <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                    El jadida
-                  </h3>
-                  <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Break time (20 minutes), 10:15
-                  </time>
-                </li>
-                <li className="ml-6">
-                  <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <BiBus
-                      className="text-blue-600 dark:text-blue-400"
-                      size={15}
-                    />
-                  </span>
-                  <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
-                    To Casablanca
-                  </h3>
-                  <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                    Arrival date, 14:15
-                  </time>
-                </li>
-              </ol>
-            </div> */}
-            {/* tt */}
-            <div>
-              <ol className="items-center sm:flex">
-                <li className="relative mb-6 sm:mb-0">
-                  <div className="flex items-center">
-                    <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                      <BiBus
-                        className="text-blue-600 dark:text-blue-400"
-                        size={15}
-                      />
-                    </span>
-                    <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                  </div>
-                  <div className="mt-3 sm:pr-8">
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                      Safi
-                    </h3>
-                    <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                      Departure time, 08:15
-                    </time>
-                  </div>
-                </li>
-                <li className="relative mb-6 sm:mb-0">
-                  <div className="flex items-center">
-                    <div className="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                      <svg
-                        aria-hidden="true"
-                        className="w-3 h-3 text-blue-600 dark:text-blue-300"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </div>
-                    <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                  </div>
-                  <div className="mt-3 sm:pr-8">
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                      
-                    </h3>
-                    <time className="flex mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                      <AiOutlineClockCircle/>, 6h
-                    </time>
-                  </div>
-                </li>
-                <li className="relative mb-6 sm:mb-0">
-                  <div className="flex items-center">
-                    <span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                      <BiBus
-                        className="text-blue-600 dark:text-blue-400"
-                        size={15}
-                      />
-                    </span>
-                    {/* <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div> */}
-                  </div>
-                  <div className="mt-3 sm:pr-8">
-                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                      Casablanca
-                    </h3>
-                    <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                      Arrival time, 13:15
-                    </time>
-                  </div>
-                </li>
-              </ol>
+            <div className="flex items-center space-x-4 my-3">
+              <div className="flex flex-col items-center">
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  08:15
+                </h3>
+                <time className="block  text-sm font-normal  leading-none text-gray-400 dark:text-gray-500">
+                  Safi Station.
+                </time>
+              </div>
+              <div className="hidden sm:flex w-20 bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+              <div className="flex items-center">
+                <time className="flex text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  <AiOutlineClockCircle /> <span className="pl-2">6h</span>
+                </time>
+              </div>
+              <div className="hidden sm:flex w-20 bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+              <div className="flex flex-col items-center ">
+                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  13:15
+                </h3>
+                <time className="block text-sm font-normal  leading-none text-gray-400 dark:text-gray-500">
+                  Casablanca Station.
+                </time>
+              </div>
             </div>
+            <div className="flex justify-between items-center space-x-4"></div>
+          </div>
+          <div className="flex w-40 justify-center items-center">
+            <h1 className="text-3xl text-orange-400">80DH</h1>
           </div>
         </div>
       </div>
-      <NewCard/>
+      {/* <NewCard /> */}
     </>
   );
 };
--- code/client/src/components/Bus Card/new.jsx
+++ code/client/src/components/Bus Card/new.jsx
@@ -1,10 +1,54 @@
 import React from "react";
+import Ctm from "../../assets/bus/ctm1.jpg";
+import { BiBus } from "react-icons/bi";
+import { AiOutlineClockCircle } from "react-icons/ai";
 
 const newCard = () => {
   return (
     <>
-      <div className="flex justify-center w-1/2 bg-slate-300">
-        <h1>ggggg</h1>
+      <div className="flex justify-between border-x w-2/3 bg-slate-3 rounded-lg bg-white shadow-lg">
+        <div className="w-20">
+          <img
+            className="w-full md:h-auto object-cover rounded-t-lg md:rounded-none md:rounded-l-lg"
+            src={Ctm}
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col p-2">
+          <div className="flex px-1 items-center space-x-2 rounded-sm  ">
+            <BiBus size={22} />
+            <span className="text-black text-xl font-semibold">
+              CTM Company
+            </span>
+          </div>
+          <div className="flex items-center w-full">
+            <div className="flex items-center space-x-4">
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                08:15
+              </h3>
+              <div className="hidden sm:flex w-20 bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+              <div className="flex items-center">
+                <time className="flex text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  <AiOutlineClockCircle /> <span className="pl-2">6h</span>
+                </time>
+              </div>
+              <div className="hidden sm:flex w-20 bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                13:15
+              </h3>
+            </div>
+            {/* <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                Safi Station.
+              </time> */}
+            <div>
+
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div>
+            ffff
+        </div>
       </div>
     </>
   );
